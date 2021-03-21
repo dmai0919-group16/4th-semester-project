8fa175df0cdd17c89660f37ed9fd4ea7f9396634
Krisztián Henrik Papp <43473144+henkyfen@users.noreply.github.com>
2021-03-21 09:33:59 +0100

    Create spotify_helper with user authentication methods

--- spotify_export/spotify_helper.py
+++ spotify_export/spotify_helper.py
@@ -1,6 +1,24 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import json
-import csv
+import uuid
 
-def 
+def get_spotify_oauth(scope, client_id, client_secret, redirect_uri, cache_handler=spotipy.cache_handler.CacheFileHandler):
+    return SpotifyOAuth(
+        scope=scope, 
+        client_id=client_id, 
+        client_secret=client_secret, 
+        redirect_uri=redirect_uri, 
+        cache_handler=cache_handler)
+
+
+def authenticate_user_get_url(spotify_oauth):
+    return spotify_oauth.get_authorize_url()
+
+
+def authenticate_user_get_token(spotify_oauth, url):
+    code = spotify_oauth.parse_response_code(url)
+    if code != url:
+        token = spotify_oauth.get_access_token(code)
+        return token
+    else:
+        raise Exception("The URL is invalid or does not contain a valid response code.")
